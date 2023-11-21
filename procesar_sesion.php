60a6114de46ebbf3d48ffad2fd630cd878b6efd3
FloresEdd <e.flores27nov@hotmail.com>
2023-11-20 23:15:09 -0600

    procesar_sesion.php modified

--- procesar_sesion.php
+++ procesar_sesion.php
@@ -1,30 +1,23 @@
 <?php
-// Configuración de la conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "saeko";
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
+//file that cointain the connection to the database
+require "db/conexion.php";
+// verify the connection
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Recuperar datos del formulario
+// get the data of form
 $username = $_POST['username'];
 $apellido = $_POST['apellido'];
 $password = $_POST['password'];
 $matricula = $_POST['matricula'];
 $correo = $_POST['correo'];
 
-// Verificar si el usuario ya existe en la base de datos
+// check if user exists
 $query = "SELECT * FROM usuarios WHERE username='$username'";
 $result = $conn->query($query);
 
-    // El usuario no existe, registrar en la base de datos
+    // if user dont exist, register a new
     $insertQuery = "INSERT INTO usuarios (username, apellido, password, matricula, correo) VALUES ('$username', '$apellido', '$password', '$matricula', '$correo')";
     if ($conn->query($insertQuery) === TRUE) {
         header("location: index.html");
@@ -33,6 +26,6 @@ $result = $conn->query($query);
         echo "Error al registrar: " . $conn->error;
     }
 
-// Cerrar la conexión a la base de datos
+// close the db connection
 $conn->close();
 ?>
