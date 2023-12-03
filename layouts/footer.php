4e4aca50d78784a432da0a4ebc115e9e7256d855
FloresEdd <e.flores27nov@hotmail.com>
2023-12-03 17:12:52 -0600

    folder with nabvar and footer

---
+++ layouts/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+        <div class="redes">
+        Nuestras redes sociales:<br>
+        
+        </div>
+        <div class="contacto">
+
+        </div>
+    </div>
+</footer>
