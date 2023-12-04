ec8a788b7986a92fef08a0a1cb0d69ae29f508a3
FloresEdd <e.flores27nov@hotmail.com>
2023-12-03 21:36:43 -0600

    footer ready

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,11 +1,94 @@
-<footer>
-    <div class="container">
-        <div class="redes">
-        Nuestras redes sociales:<br>
-        
-        </div>
-        <div class="contacto">
+<style>
+    footer {
+  background-color: #4CAF50;
+  color: black;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+ footer a {
+    color: black;
+  transition: transform 0.3s ease;
+}
 
+.redes {
+  flex-basis: 33%;
+  text-align: left;
+  margin-bottom: 10px;
+}
+
+.redes img {
+  width: 70px;
+}
+
+.nosotros {
+  flex-basis: 33%;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.descargas {
+  flex-basis: 33%;
+  text-align: right;
+  margin-bottom: 20px;
+}
+
+.descargas h3 {
+  margin-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  position: relative;
+  top: -30px;
+}
+.nosotros h3{
+margin-bottom: 10px;
+font-size: 18px;
+font-weight: bold;
+position: relative;
+top: -100px;
+}
+.nosotros a{
+position: relative;
+top: -100px;
+}
+
+.descargas img {
+  width: 80px;
+  height: auto;
+  margin-top: 10px;
+}
+ footer a:hover {
+    color: white;
+  transform: rotate(360deg) scale(1.2);
+}
+</style>
+<footer>
+        <div class="redes">
+            Nuestras redes sociales:<br>
+        <div class="facebook">
+            <a href="https://www.facebook.com/SaekoEscolar/"> <img src="../images/facebook.png" alt="facebook"> </a>
         </div>
-    </div>
+        <div class="twitter">
+            <a href="https://twitter.com/SaekoEscolar"> <img src="../images/twitter.png" alt="twitter"> </a>
+        </div>
+        <div class="instagram">
+            <a href="https://www.instagram.com/saekoescolar/"> <img src="../images/instagram.png" alt="instagram"></a>
+        </div>
+        <div class="youtube">
+            <a href="https://www.youtube.com/c/SaekoIo?sub_confirmation=1"> <img src="../images/youtube.png" alt="youtube"></a>
+        </div>
+        </div>  
+        <div class="nosotros">
+            <h3>Nosotros:</h3><br>
+            <a href="https://www.saeko.io/es/nosotros">Sobre nosotros</a><br>
+            <a href="https://www.saeko.io/es/blog"> Noticias </a><br>
+        </div>
+        <div class="descargas">
+            <h3>Descarga Saeko:</h3><br>
+            Descargalo para <a href="https://apps.apple.com/mx/app/saeko/id1477257337"> <img src="../images/apple.png" alt="apple"></a><br>
+            Descargalo para <a href="https://play.google.com/store/apps/details?id=com.kioru.app.saeko"> <img src="../images/android.png" alt="android"> </a>
+        </div>
+    
 </footer>
