a5f71f4b37bd241d9bca55d51e8be23f8e6c6a67
charmander554k <132737008+charmander554k@users.noreply.github.com>
2023-12-03 20:13:00 -0600

    Update alumnos.php

--- alumnos.php
+++ alumnos.php
@@ -58,3 +58,15 @@ $conn->close();
 echo "</body>";
 echo "</html>";
 ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Inicio de sesión</title>
+</head>
+<body class="backcolor2"></body>
+</html>
+
